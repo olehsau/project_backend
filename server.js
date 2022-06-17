4669fe0f81a66babbaffd0d5e052e1567c48fe66
trololzen <trololzen@gmail.com>
2022-06-17 17:46:10 +0200

    added registration and logging in

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var mongoose = require('mongoose');
 
 var dbUrl = 'mongodb+srv://olehsau:65138@cluster0.lie2rmp.mongodb.net/test'
 
-var Message = mongoose.model('Message', {name : String, message : String})
+var Message = mongoose.model('messages', {name : String, message : String})
+var User = mongoose.model('users', {name : String, password : String})
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
@@ -51,6 +52,40 @@ app.post('/messages', (req, res) => {
   });
 
 
-  server.listen(3005, () => {
+  // auth
+
+var currentUsersName = "anonimous";
+
+app.post('/register', (req, res) => {
+    var user = new User(req.body);
+    user.save((err) =>{
+      if(err)
+        sendStatus(500);
+      res.sendStatus(200);
+    })
+  })
+
+  app.post('/login', (req, res) => {
+    var entered_user = new User(req.body);
+    User.find({name : entered_user.name}, (err,found_user)=>{
+        if(err){console.log(err);}
+        else{
+            console.log("needed password: "+ found_user[0].password);
+            console.log("entered password: "+ entered_user.password);
+            if(entered_user.password == found_user[0].password){
+                console.log("good password");
+                currentUsersName = entered_user.name;
+                res.send(currentUsersName);
+            }
+            else{
+                console.log("bad password");
+            }
+        }
+    })
+  })
+
+//
+
+server.listen(3005, () => {
     console.log('listening on *:3005');
   });
